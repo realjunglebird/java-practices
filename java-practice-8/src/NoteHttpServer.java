acc8c4cb545d42c6e94458cb0bb82d9f2bdb4d5b
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-12-16 01:09:33 +0300

    upd7

---
+++ java-practice-8/src/NoteHttpServer.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class NoteHttpServer {
+    private static final int PORT = 8080;
+    private static final List<String> notes = new ArrayList<>();
+
+}
